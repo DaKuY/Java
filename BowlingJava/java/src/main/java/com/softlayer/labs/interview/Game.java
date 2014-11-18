a3a390e53aafbc67ef590cce4818d8af2132c2e8
Andrew Camero <drewe927@gmail.com>
2014-11-17 19:29:47 -0600

    updated bowling game to be modular

--- BowlingJava/java/src/main/java/com/softlayer/labs/interview/Game.java
+++ BowlingJava/java/src/main/java/com/softlayer/labs/interview/Game.java
@@ -4,11 +4,11 @@ public class Game
 {
 	
 	private Integer[] currentScore = new Integer[22];
-	private int frame;
+	private int rollNumberIndex;
 	private int finalScore;
 	
 	Game(){
-		frame = 1;
+		rollNumberIndex = 1;
 		finalScore = 0;
 		init();
 	}
@@ -23,79 +23,94 @@ public class Game
     // Called for each roll of the bowling ball. Passed the number
     // of pins knocked down on this roll.
     public void roll(int pins) {
-    	//currentScore += pins;
+    	    	
+    	/** Final rollNumberIndex **/
+    	if (rollNumberIndex == 21){
+    		currentScore[rollNumberIndex] = pins;
+    	}
     	
+    	else{
+    		checkPinCount(pins);
+    	}
+    	
+    	
+    }
     
+    public void checkPinCount(int pins){
     	
-    	/** Final Frame **/
-    	if (frame == 21){
-    		currentScore[frame] = pins;
-    	}
-    	
-    	/** Spare **/
-    	else if (frame % 2 != 0 && 
+    	/** Spare or previous frame was a spare**/
+    	if (rollNumberIndex % 2 != 0 && 
     			pins != 10 && 
-    			frame > 2 &&
-    			currentScore[frame - 2] + currentScore[frame - 1] == 10 &&
-    			currentScore[frame - 2] != 10 ){
+    			rollNumberIndex > 2 &&
+    			currentScore[rollNumberIndex - 2] + currentScore[rollNumberIndex - 1] == 10 &&
+    			currentScore[rollNumberIndex - 2] != 10 ){
     		
-    		//check if the last frame was a strike
-    		if (currentScore[frame - 3] == 10){
-    			currentScore[frame - 3] += currentScore[frame - 1] + currentScore[frame];
-    		}else{
-        		//split - adds pins to previous frame
-    			currentScore[frame-1] += pins;
-    		}
-    		
-    		//sets current frame
-    		currentScore[frame] = pins;    	
+    		addSpareScore(pins);
     		
     	}
     	
-    	/** strike **/
-	    		
-		// previous frame was a strike
-    	else if (frame > 2 && currentScore[frame - 3] == 10){
-    		//strike - adds pins to previous frame
-    		currentScore[frame - 3] = currentScore[frame - 3] + currentScore[frame - 1] + pins;
-    		    		    		
-    		//sets current frame
-    		currentScore[frame] = pins;
+    	/** Strike or previous frame was a strike **/
+    	else if(rollNumberIndex > 2 && (currentScore[rollNumberIndex - 3] == 10 || currentScore[rollNumberIndex - 1] == 10 || currentScore[rollNumberIndex - 2] == 10 || pins == 10)){
+    		addStrikeScore(pins);
     	}
-		
-		// strike and the previous frame was a spare
-		else if (frame > 2 && currentScore[frame - 2] + currentScore[frame - 1] == 10 && currentScore[frame - 2] != 10 && pins == 10) {
-    		
-    		//check if the last frame was a spare
-    		if (currentScore[frame - 2] + currentScore[frame - 1] == 10){
-    			currentScore[frame - 2] += pins;
-    		}
-    		//sets current frame
-    		currentScore[frame] = pins;
-    		
-    	}else if (frame > 4 && currentScore[frame - 2] == 10 && pins == 10){
-    		//strike - adds pins to previous frame
-    		currentScore[frame - 2] += pins + pins;
-    		//sets current frame
-    		currentScore[frame] = pins;
-
-    	}else if (frame > 2 && currentScore[frame - 2] == 10 && pins == 10){
-    		//strike - adds pins to previous frame
-    		currentScore[frame - 2] += pins;
-    		//sets current frame
-    		currentScore[frame] = pins;
-    	}
-    		
     	
     	/** anything else - not hitting spares or strikes **/
     	else{
-    		currentScore[frame] = pins;
+    		currentScore[rollNumberIndex] = pins;
     	}
     	
-    	getScore();
-    	
-    	frame ++;
+    	rollNumberIndex ++;
     }
+    
+    public void addSpareScore(int pins){
+    	
+    	//check if the last rollNumberIndex was a strike
+		if (currentScore[rollNumberIndex - 3] == 10){
+			currentScore[rollNumberIndex - 3] += currentScore[rollNumberIndex - 1] + currentScore[rollNumberIndex];
+		}else{
+    		//split - adds pins to previous rollNumberIndex
+			currentScore[rollNumberIndex-1] += pins;
+		}
+		
+		//sets current rollNumberIndex
+		currentScore[rollNumberIndex] = pins;   
+    }
+    
+    public void addStrikeScore(int pins){
+    	// previous rollNumberIndex was a strike
+    	if (rollNumberIndex > 2 && currentScore[rollNumberIndex - 3] == 10){
+    		//strike - adds pins to previous rollNumberIndex
+    		currentScore[rollNumberIndex - 3] = currentScore[rollNumberIndex - 3] + currentScore[rollNumberIndex - 1] + pins;
+    		    		    		
+    		//sets current rollNumberIndex
+    		currentScore[rollNumberIndex] = pins;
+    	}
+		
+		// strike and the previous rollNumberIndex was a spare
+		else if (rollNumberIndex > 2 && currentScore[rollNumberIndex - 2] + currentScore[rollNumberIndex - 1] == 10 && currentScore[rollNumberIndex - 2] != 10 && pins == 10) {
+    		
+    		//check if the last rollNumberIndex was a spare
+    		if (currentScore[rollNumberIndex - 2] + currentScore[rollNumberIndex - 1] == 10){
+    			currentScore[rollNumberIndex - 2] += pins;
+    		}
+    		//sets current rollNumberIndex
+    		currentScore[rollNumberIndex] = pins;
+    		
+    	}else if (rollNumberIndex > 4 && currentScore[rollNumberIndex - 2] == 10 && pins == 10){
+    		//strike - adds pins to previous rollNumberIndex
+    		currentScore[rollNumberIndex - 2] += pins + pins;
+    		//sets current rollNumberIndex
+    		currentScore[rollNumberIndex] = pins;
+
+    	}else if (rollNumberIndex > 2 && currentScore[rollNumberIndex - 2] == 10 && pins == 10){
+    		//strike - adds pins to previous rollNumberIndex
+    		currentScore[rollNumberIndex - 2] += pins;
+    		//sets current rollNumberIndex
+    		currentScore[rollNumberIndex] = pins;
+    	}
+    }
+    
+    
 
     // Called once at the end of the game.
     public int getScore() {
